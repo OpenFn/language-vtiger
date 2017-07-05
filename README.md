5605555b220aa1b95b768eb42804003e5ec72c68
Taylor Downs <downs.taylor@gmail.com>
2017-07-05 02:08:15 -0400

    working with hosted vtiger

--- README.md
+++ README.md
@@ -12,69 +12,28 @@ Documentation
   "configuration": {
     "hostUrl": "https://openfunction.od2.vtiger.com",
     "username": "taylor@openfn.org",
-    "accessToken": "4VRSjLYUZsc4Bqvh"
+    "accessToken": "blahBlahBlah"
   }
 }
 ```
 
-#### sample createEntity expression
+#### sample listTypes expression
 ```js
-createEntity({
-  entityName: "accounts",
-  body: {
-        "name": "Open Function",
-        "creditonhold": false,
-        "address1_latitude": 47.639583,
-        "description": "This is the description of the sample account",
-        "revenue": 5000000,
-        "accountcategorycode": 1
-  }
-});
+listTypes();
 ```
 
-#### sample query expression
-*Can be used to retrieve or query a specific Entity if an entityId is declared in the expression. Otherwise it will perform a query on all data with the defined Entity Name*
+#### sample postElement expression
 ```js
-query({
-  entityName: "contacts",
-  entityId: "51a0e5b9-88df-e311-b8e5-6c3be5a8b200",
-  query: {
-    fields: [
-      'fullname',
-      'birthdate'
-    ],
-   limit: 10,
-    orderBy: {
-      field: 'lastname',
-      direction: 'asc'
-    },
-    filter: 'firstname eq \'Cat\''
+postElement({
+  operation: "create"
+  elementType: "leads",
+  element: {
+    "name": dataValue("name")(state),
+    "surname": dataValue("surname")(state),
+    "email": dataValue("email")(state)
   }
 });
-```
 
-#### sample update expression
-```js
-updateEntity({
-  entityName: "accounts",
-  entityId: "f4301865-c64a-e711-80f3-e0071b6fd061",
-  body: {
-        "name": "wilder 2",
-        "creditonhold": true,
-        "address1_latitude": 47.639583,
-        "description": "This is the description of the sample account",
-        "revenue": 5000000,
-        "accountcategorycode": 1
-  }
-});
-```
-
-#### sample delete expression
-```js
-deleteEntity({
-  entityName: "accounts",
-  entityId: "aeef941b-254a-e711-80f1-e0071b685921"
-});
 ```
 
 [Docs](docs/index)
--- src/Adaptor.js
+++ src/Adaptor.js
@@ -1,6 +1,15 @@
 import { execute as commonExecute, expandReferences } from 'language-common';
 import request from 'request';
+import md5 from 'md5';
+// var md5 = require('md5');
 import { resolve as resolveUrl } from 'url';
+import { curry, mapValues, flatten } from 'lodash-fp';
+
+function assembleError({ response, error }) {
+  if (response && ([200,201,202,204].indexOf(response.statusCode) > -1)) return false;
+  if (error) return error;
+  return new Error(`Server responded with ${response.statusCode}`)
+}
 
 /** @module Adaptor */
 
@@ -22,217 +31,136 @@ export function execute(...operations) {
     data: null
   }
 
+  // return state => {
+  //   return commonExecute(...operations)({ ...initialState, ...state })
+  // };
+
   return state => {
-    return commonExecute(...operations)({ ...initialState, ...state })
+    return commonExecute(
+      challenge,
+      login,
+      ...flatten(operations)
+    )({ ...initialState, ...state })
+
   };
 
 }
 
+function challenge(state) {
 
-export function createEntity(params) {
+  const {hostUrl, username, accessToken} = state.configuration
+
+  console.info(`Challenging as ${username}...`);
+
+  return new Promise((resolve, reject) => {
+    request.get (`${hostUrl}/webservice.php?operation=getchallenge&username=${username}`,
+      function(error, response, body){
+      error = assembleError({response, error})
+      if(error) {
+        reject(error);
+      } else {
+        console.log("Challenge succeeded.");
+        body = JSON.parse(body)
+        resolve(body.result.token);
+      }
+    })
+  })
+  .then((value) => {
+    return { ...state, token: value }
+  })
+
+}
+
+export function login(state) {
+
+  const {hostUrl, username, accessToken} = state.configuration;
+
+  const { token } = state;
+
+  console.info(`Logging in...`)
+
+  return new Promise((resolve, reject) => {
+    request.post ({
+      url: `${hostUrl}/webservice.php`,
+      form: {
+        operation: 'login',
+        username: username,
+        accessKey: md5(token + accessToken)
+      }
+    }, function(error, response, body){
+      error = assembleError({response, error})
+      if(error) {
+        console.log(response)
+        reject(error);
+      } else {
+        console.log("Login succeeded.");
+        body = JSON.parse(body)
+        resolve(body);
+      }
+    })
+  })
+  .then((result) => {
+    return { ...state, session: result }
+  })
+
+}
+
+export function listTypes() {
+  return state => {
+
+    const { hostUrl } = state.configuration;
+    const { sessionName } = state.session.result;
+
+    return new Promise((resolve, reject) => {
+      request.post ({
+        url: `${hostUrl}/webservice.php`,
+        form: { operation: 'listTypes', sessionName }
+      }, function(error, response, body){
+        error = assembleError({response, error})
+        if(error) {
+          reject(error);
+        } else {
+          console.log(body)
+          resolve(body);
+        }
+      })
+    }).then((data) => {
+      const nextState = { ...state, response: { body: data } };
+      return nextState;
+    })
+
+  }
+}
+
+export function postElement(params) {
 
   return state => {
 
-    function assembleError({ response, error }) {
-      if (response && ([200,201,202,204].indexOf(response.statusCode) > -1)) return false;
-      if (error) return error;
-      return new Error(`Server responded with ${response.statusCode}`)
+    const { hostUrl } = state.configuration;
+    const { sessionName } = state.session.result;
+
+    const { elementType, element, operation } = expandReferences(params)(state);
+
+    const url = `${hostUrl}/webservice.php`;
+
+    console.log(`Performing a(n) ${operation} on ${elementType}.`);
+
+    const body = {
+      operation,
+      sessionName,
+      element,
+      elementType
     }
-
-    const { resource, accessToken, apiVersion } = state.configuration;
-
-    const { entityName, body } = expandReferences(params)(state);
-
-    const url = `${resource}/api/data/v${apiVersion}/${entityName}`;
-
-    const headers = {
-      'OData-MaxVersion': '4.0',
-      'OData-Version': '4.0',
-      'Content-Type': 'application/json',
-      'Authorization': accessToken
-    };
-
-    console.log("Posting to url: " + url);
-    console.log("With body: " + JSON.stringify(body, null, 2));
-
 
     return new Promise((resolve, reject) => {
       request.post ({
         url: url,
-        json: body,
-        headers
+        form: body
       }, function(error, response, body){
         error = assembleError({response, error})
         if(error) {
           reject(error);
         } else {
-          console.log("Create entity succeeded.");
-          console.log(body)
-          resolve(body);
-        }
-      })
-    }).then((data) => {
-      const nextState = { ...state, response: { body: data } };
-      return nextState;
-    })
-
-  };
-
-};
-
-export function query(params) {
-
-  return state => {
-
-    function assembleError({ response, error }) {
-      if (response && ([200,201,202,204].indexOf(response.statusCode) > -1)) return false;
-      if (error) return error;
-      return new Error(`Server responded with ${response.statusCode}`)
-    }
-
-    const { resource, accessToken, apiVersion } = state.configuration;
-    const { entityName, entityId, query } = expandReferences(params)(state);
-
-    const url = `${resource}/api/data/v${apiVersion}/${entityName}`;
-
-    const urlId = ( entityId ? `${url}(${entityId})` : url );
-
-    // TODO: find a better way of running these ternaries.
-    // Here we initialize an empty object if no query is present.
-    const ternaryQuery = query || {};
-
-    const selectors = ( ternaryQuery.fields ? `$select=${query.fields.join(',')}` : null );
-    const orderBy = ( ternaryQuery.orderBy ? `$orderby=${query.orderBy.field} ${query.orderBy.direction}` : null );
-    const filter = ( ternaryQuery.filter ? `$filter=${query.filter}` : null );
-    const limit = ( ternaryQuery.limit ?  query.limit : 0 );
-
-    const queryUrl = [selectors, orderBy, filter]
-                      .filter( i => {
-                        return i != null
-                      })
-                      .join('&');
-
-    const fullUrl = ( queryUrl ? `${urlId}?${queryUrl}` : urlId );
-
-    console.log("Full URL: " + fullUrl);
-
-    const headers = {
-      'OData-MaxVersion': '4.0',
-      'OData-Version': '4.0',
-      'Content-Type': 'application/json',
-      'Authorization': accessToken,
-      'Prefer': 'odata.maxpagesize=' + limit
-    };
-
-    return new Promise((resolve, reject) => {
-      request.get ({
-        url: fullUrl,
-        headers
-      }, function(error, response, body){
-        error = assembleError({response, error})
-        if(error) {
-          reject(error);
-        } else {
-          console.log("Query succeeded.");
-          console.log(body)
-          resolve(body);
-        }
-      })
-    }).then((data) => {
-      const nextState = { ...state, response: { body: data } };
-      return nextState;
-    })
-
-  };
-
-};
-
-export function updateEntity(params) {
-
-  return state => {
-
-    function assembleError({ response, error }) {
-      if (response && ([200,201,202,204].indexOf(response.statusCode) > -1)) return false;
-      if (error) return error;
-      return new Error(`Server responded with ${response.statusCode}`)
-    }
-
-    const { resource, accessToken, apiVersion } = state.configuration;
-
-    const { entityName, entityId, body } = expandReferences(params)(state);
-
-    const url = `${resource}/api/data/v${apiVersion}/${entityName}(${entityId})`;
-
-    const headers = {
-      'OData-MaxVersion': '4.0',
-      'OData-Version': '4.0',
-      'Content-Type': 'application/json',
-      'Authorization': accessToken
-    };
-
-    console.log("Posting to url: " + url);
-    console.log("With body: " + JSON.stringify(body, null, 2));
-
-
-    return new Promise((resolve, reject) => {
-      request.patch ({
-        url: url,
-        json: body,
-        headers
-      }, function(error, response, body){
-        error = assembleError({response, error})
-        if(error) {
-          reject(error);
-        } else {
-          console.log("Update succeeded.");
-          console.log(body)
-          resolve(body);
-        }
-      })
-    }).then((data) => {
-      const nextState = { ...state, response: { body: data } };
-      return nextState;
-    })
-
-  };
-
-};
-
-export function deleteEntity(params) {
-
-  return state => {
-
-    function assembleError({ response, error }) {
-      if (response && ([200,201,202,204].indexOf(response.statusCode) > -1)) return false;
-      if (error) return error;
-      return new Error(`Server responded with ${response.statusCode}`)
-    }
-
-    const { resource, accessToken, apiVersion } = state.configuration;
-
-    const { entityName, entityId } = expandReferences(params)(state);
-
-    const url = `${resource}/api/data/v${apiVersion}/${entityName}(${entityId})`;
-
-    const headers = {
-      'OData-MaxVersion': '4.0',
-      'OData-Version': '4.0',
-      'Content-Type': 'application/json',
-      'Authorization': accessToken
-    };
-
-    console.log("Posting to url: " + url);
-
-    return new Promise((resolve, reject) => {
-      request.delete ({
-        url: url,
-        headers
-      }, function(error, response, body){
-        error = assembleError({response, error})
-        if(error) {
-          reject(error);
-        } else {
-          console.log("Delete succeeded.");
           console.log(body)
           resolve(body);
         }
